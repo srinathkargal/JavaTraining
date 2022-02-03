b906ab9f7c26a412a974f72733487bb30673e345
Srinath K L <srinathkl.rao@gmail.com>
2022-02-03 20:55:50 +0530

    hello_world.java

---
+++ hello_world.java
@@ -0,0 +1,15 @@
+// Your First Program
+
+class HelloWorld {
+  // create a method
+  public int addNumbers(int a, int b) {
+    int sum = a + b;
+    // return value
+    return sum;
+  }
+  
+  public static void main(String[] args) {
+      System.out.println("Hello, World!"); 
+  }
+}
+
